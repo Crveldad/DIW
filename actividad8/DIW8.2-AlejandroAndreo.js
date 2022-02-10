104bc9cb2ac8f50d2b05b92248a52f5b243612e8
Crveldad <crveldad@hotmail.com>
2022-02-10 20:38:57 +0100

    actividad 8-3 done

--- actividad8/DIW8.2-AlejandroAndreo.js
+++ actividad8/DIW8.2-AlejandroAndreo.js
@@ -6,4 +6,31 @@ $(document).ready(function() {
         $(".caja1").slideUp(300);
     });
 
+    $("#b2").click(function() {
+        $(".caja1").slideDown(300);
+    });
+
+    $("#b3").click(function() {
+        $(".caja1").slideToggle(300);
+    });
+
+    $("#b4").click(function() {
+        $(".caja1").fadeOut(300);
+    });
+
+    $("#b5").bind("click mouseenter",
+        function() {
+            $(".caja1").css("background-color", "green");
+        });
+
+    $("#b6").click(function() {
+        $(".caja1").fadeIn(100);
+        $(".caja1").delay(800);
+        $(".caja1").fadeOut(800);
+    });
+
+    $("#b7").click(function() {
+        $(".caja1").toggleClass("animacion");
+    });
+
 });
